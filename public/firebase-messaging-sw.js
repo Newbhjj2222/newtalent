6a9f7330d1f029d07282af0751eaa02074cc9fbb
NewtalentsG <newtalents403@gmail.com>
2025-08-19 09:56:41 +0200

    Create firebase-messaging-sw.js

---
+++ public/firebase-messaging-sw.js
@@ -0,0 +1,22 @@
+importScripts('https://www.gstatic.com/firebasejs/9.23.0/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/9.23.0/firebase-messaging-compat.js');
+
+firebase.initializeApp({
+  apiKey: "AIzaSyAvwNyhKiKFyI-r6MDDk7BH3iq7P61z594",
+  authDomain: "newtalents-a7c29.firebaseapp.com",
+  projectId: "newtalents-a7c29",
+  storageBucket: "newtalents-a7c29.appspot.com",
+  messagingSenderId: "507408992610",
+  appId: "1:507408992610:web:05ce220a4cb4922de9843b",
+  measurementId: "G-XZVMTFEQBE"
+});
+
+const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage((payload) => {
+  const notificationTitle = payload.notification.title;
+  const notificationOptions = {
+    body: payload.notification.body,
+  };
+  self.registration.showNotification(notificationTitle, notificationOptions);
+});

b7b8bac07fa62956e482176416bfedb0854acfd2
NewtalentsG <newtalents403@gmail.com>
2025-08-23 00:20:17 +0200

    Update firebase-messaging-sw.js

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,5 +1,6 @@
-importScripts('https://www.gstatic.com/firebasejs/9.23.0/firebase-app-compat.js');
-importScripts('https://www.gstatic.com/firebasejs/9.23.0/firebase-messaging-compat.js');
+/* eslint-disable no-undef */
+importScripts("https://www.gstatic.com/firebasejs/9.0.0/firebase-app.js");
+importScripts("https://www.gstatic.com/firebasejs/9.0.0/firebase-messaging.js");
 
 firebase.initializeApp({
   apiKey: "AIzaSyAvwNyhKiKFyI-r6MDDk7BH3iq7P61z594",
@@ -7,16 +8,16 @@ firebase.initializeApp({
   projectId: "newtalents-a7c29",
   storageBucket: "newtalents-a7c29.appspot.com",
   messagingSenderId: "507408992610",
-  appId: "1:507408992610:web:05ce220a4cb4922de9843b",
-  measurementId: "G-XZVMTFEQBE"
+  appId: "1:507408992610:web:05ce220a4cb4922de9843b"
 });
 
 const messaging = firebase.messaging();
 
+// Notifications igihe site ifunze cyangwa iri background
 messaging.onBackgroundMessage((payload) => {
-  const notificationTitle = payload.notification.title;
-  const notificationOptions = {
+  console.log("Background message ", payload);
+  self.registration.showNotification(payload.notification.title, {
     body: payload.notification.body,
-  };
-  self.registration.showNotification(notificationTitle, notificationOptions);
+    icon: "/logo.png" // shyiramo icon yawe
+  });
 });

305374f8fc8de838f096fd2b591edf08ca4628f9
NewtalentsG <newtalents403@gmail.com>
2025-08-29 18:37:39 +0200

    Update hello.js

--- src/pages/api/hello.js
+++ src/pages/api/hello.js
@@ -1,5 +1,9 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-
-export default function handler(req, res) {
-  res.status(200).json({ name: "John Doe" });
+export default async function handler(req, res) {
+  try {
+    // code yawe
+    res.status(200).json({ message: "ok" });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Server error" });
+  }
 }

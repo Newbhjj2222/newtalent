1a7180a87064c95ef59718feec2d2eb60d629f8f
NewtalentsG <newtalents403@gmail.com>
2025-08-30 07:19:09 +0200

    Update _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,21 @@
+// pages/_app.js
 import "@/styles/globals.css";
+import Script from "next/script";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      {/* Google AdSense script global */}
+      <Script
+        id="adsense-script"
+        async
+        strategy="afterInteractive"
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2059573562083386"
+        crossOrigin="anonymous"
+      />
+
+      {/* Pages zose zishyirwa hano */}
+      <Component {...pageProps} />
+    </>
+  );
 }

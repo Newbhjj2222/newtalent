dba9b863e271cc8b2d8c1275155f57b0d6fb47d6
NewtalentsG <newtalents403@gmail.com>
2025-08-30 09:34:18 +0200

    Update _document.js

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,7 +3,12 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        {/* Favicon/logo */}
+        <link rel="icon" href="/logo.png" />
+        {/* Title y'urubuga */}
+        <title>New Talents Stories Group</title>
+      </Head>
       <body>
         <Main />
         <NextScript />

8d3fb9dc1f6d57bf7abf86d80e9e707e10cbcad8
NewtalentsG <newtalents403@gmail.com>
2025-09-16 00:01:23 +0200

    Update _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -3,26 +3,35 @@ import "@/styles/globals.css";
 import Script from "next/script";
 import Head from "next/head";
 
+// Import language provider
+import { LanguageProvider } from "../components/LanguageProvider";
+
 export default function App({ Component, pageProps }) {
   return (
-    <>
-      {/* Title ya website */}
-      <Head>
-        <title>New Talents Stories Group</title>
-        <link rel="icon" href="/logo.png" />
-      </Head>
+    <LanguageProvider>
+      <>
+        {/* Title ya website */}
+        <Head>
+          <title>New Talents Stories Group</title>
+          <link rel="icon" href="/logo.png" />
+        </Head>
 
-      {/* Google AdSense script global */}
-      <Script
-        id="adsense-script"
-        async
-        strategy="afterInteractive"
-        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2059573562083386"
-        crossOrigin="anonymous"
-      />
+        {/* Google AdSense script global */}
+        <Script
+          id="adsense-script"
+          async
+          strategy="afterInteractive"
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2059573562083386"
+          crossOrigin="anonymous"
+        />
 
-      {/* Pages zose zishyirwa hano */}
-      <Component {...pageProps} />
-    </>
+        {/* Language selector global (ushobora gushyira kuri navbar) */}
+        {/* Example: use in layout or top of each page */}
+        {/* <LanguageSelector /> */}
+
+        {/* Pages zose zishyirwa hano */}
+        <Component {...pageProps} />
+      </>
+    </LanguageProvider>
   );
-}
+    }

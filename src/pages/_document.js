a3e8a557720023fedaf4ceb3716b3174081d463f
NewtalentsG <newtalents403@gmail.com>
2025-09-21 14:38:21 +0200

    Update _document.js

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -9,12 +9,7 @@ export default function Document() {
         <link rel="icon" href="/logo.png" />
 
         {/* PropellerAds Global Tag */}
-        <script
-          src="https://fpyf8.com/88/tag.min.js"
-          data-zone="172619"           // Shyiramo ID yawe ya Push / Banner zone
-          async
-          data-cfasync="false"
-        ></script>
+        
       </Head>
       <body>
         <Main />

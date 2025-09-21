f16c87580d030e598da45d6a72f688e3d2b92bf3
NewtalentsG <newtalents403@gmail.com>
2025-09-21 13:47:22 +0200

    Update _document.js

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,19 +1,32 @@
-// src/pages/_document.js
 import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        {/* PropellerAds script */}
+        {/* In-Page Push (subtle & user-friendly) */}
         <script
-          dangerouslySetInnerHTML={{
-            __html: `(function(s){
-              s.dataset.zone='9909216';
-              s.src='https://forfrogadiertor.com/tag.min.js'
-            })([document.documentElement, document.body].filter(Boolean).pop().appendChild(document.createElement('script')))`
-          }}
-        />
+          src="https://fpyf8.com/88/tag.min.js"
+          data-zone="9909707"
+          async
+          data-cfasync="false"
+        ></script>
+
+        {/* Native Banner (Interstitial / subtle banner) */}
+        <script
+          src="https://fpyf8.com/88/tag.min.js"
+          data-zone="9909708"
+          async
+          data-cfasync="false"
+        ></script>
+
+        {/* Push Notifications (users opt-in) */}
+        <script
+          src="https://fpyf8.com/88/tag.min.js"
+          data-zone="9909710"
+          async
+          data-cfasync="false"
+        ></script>
       </Head>
       <body>
         <Main />

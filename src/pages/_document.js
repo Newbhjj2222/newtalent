977630bb8d57ab5ee108719aa00ae7316480fd32
NewtalentsG <newtalents403@gmail.com>
2025-09-21 14:48:40 +0200

    Update _document.js

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,15 +1,32 @@
-// pages/_document.js
 import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        {/* Favicon/logo */}
-        <link rel="icon" href="/logo.png" />
+        {/* Native Banner (subtle banner display) */}
+        <script
+          src="https://fpyf8.com/88/tag.min.js"
+          data-zone="9909708"
+          async
+          data-cfasync="false"
+        ></script>
 
-        {/* PropellerAds Global Tag */}
-        
+        {/* In-Page Push (user-friendly notifications within page) */}
+        <script
+          src="https://fpyf8.com/88/tag.min.js"
+          data-zone="9909707"
+          async
+          data-cfasync="false"
+        ></script>
+
+        {/* Push Notifications (opt-in notifications) */}
+        <script
+          src="https://fpyf8.com/88/tag.min.js"
+          data-zone="9909710"
+          async
+          data-cfasync="false"
+        ></script>
       </Head>
       <body>
         <Main />
@@ -17,4 +34,4 @@ export default function Document() {
       </body>
     </Html>
   );
-    }
+}

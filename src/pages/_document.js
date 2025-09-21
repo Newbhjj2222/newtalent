1016c8802703e9099798be7dbcb3f912502204ce
NewtalentsG <newtalents403@gmail.com>
2025-09-21 13:15:35 +0200

    Update _document.js

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,22 +1,23 @@
 // src/pages/_document.js
 import { Html, Head, Main, NextScript } from "next/document";
-import Script from "next/script";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        {/* PropellerAds script */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(s){
+              s.dataset.zone='9909216';
+              s.src='https://forfrogadiertor.com/tag.min.js'
+            })([document.documentElement, document.body].filter(Boolean).pop().appendChild(document.createElement('script')))`
+          }}
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
-
-        {/* PropellerAds global script */}
-        <Script id="propellerads" strategy="afterInteractive">
-          {`(function(s){
-              s.dataset.zone='9909216';
-              s.src='https://forfrogadiertor.com/tag.min.js'
-          })([document.documentElement, document.body].filter(Boolean).pop().appendChild(document.createElement('script')))`}
-        </Script>
       </body>
     </Html>
   );

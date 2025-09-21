1844e00f359e828574b88ca807b7ae93ed709bed
NewtalentsG <newtalents403@gmail.com>
2025-09-21 13:32:04 +0200

    Update _document.js

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,14 +1,21 @@
-// src/pages/_document.js
 import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        {/* PropellerAds script (global) */}
+        {/* In-Page Push (subtle & friendly) */}
         <script
           src="https://fpyf8.com/88/tag.min.js"
-          data-zone="172619"
+          data-zone="9909707"
+          async
+          data-cfasync="false"
+        ></script>
+
+        {/* Native Banner */}
+        <script
+          src="https://fpyf8.com/88/tag.min.js"
+          data-zone="9909708"
           async
           data-cfasync="false"
         ></script>

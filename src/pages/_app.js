be4573c63bf69222308b860f24dc95a31fb546c0
NewtalentsG <newtalents403@gmail.com>
2025-09-21 14:49:57 +0200

    Update _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,7 +2,7 @@
 import "@/styles/globals.css";
 import Script from "next/script";
 import Head from "next/head";
-import WiseAds from "../components/WiseAds";
+
 // Import language provider
 import { LanguageProvider } from "../components/LanguageProvider";
 
@@ -31,7 +31,7 @@ export default function App({ Component, pageProps }) {
 
         {/* Pages zose zishyirwa hano */}
         <Component {...pageProps} />
-          <WiseAds />
+          
       </>
     </LanguageProvider>
   );

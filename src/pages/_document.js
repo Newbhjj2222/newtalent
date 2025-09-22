a09974dc76d5bf6a9a3ee06bc46e77610f193d29
NewtalentsG <newtalents403@gmail.com>
2025-09-22 15:02:36 +0200

    Update _document.js

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,48 +1,79 @@
 // pages/_document.js
 import { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        {/* Favicon/logo */}
         <link rel="icon" href="/logo.png" />
 
         {/* Wise / PropellerAds - Banner / Push */}
-        <script
+        <Script
+          id="zone-9909216"
+          strategy="afterInteractive"
           dangerouslySetInnerHTML={{
-            __html: `(function(s){
-              s.dataset.zone='9909216',
-              s.src='https://forfrogadiertor.com/tag.min.js'
-            })([document.documentElement, document.body].filter(Boolean).pop().appendChild(document.createElement('script')))`,
-          }}
-        ></script>
-          <script
-          dangerouslySetInnerHTML={{
-            __html: `(function(s){
-              s.dataset.zone='9910308',
-              s.src='https://forfrogadiertor.com/tag.min.js'
-            })([document.documentElement, document.body].filter(Boolean).pop().appendChild(document.createElement('script')))`,
+            __html: `
+              (function(s){
+                s.dataset.zone='9909216';
+                s.src='https://forfrogadiertor.com/tag.min.js';
+              })(document.body.appendChild(document.createElement('script')));
+            `,
           }}
         />
-          <script
+        <Script
+          id="zone-9910308"
+          strategy="afterInteractive"
           dangerouslySetInnerHTML={{
-            __html: `(function(s){
-              s.dataset.zone='9915615';
-              s.src='https://forfrogadiertor.com/tag.min.js';
-            })([document.documentElement, document.body].filter(Boolean).pop().appendChild(document.createElement('script')));`,
+            __html: `
+              (function(s){
+                s.dataset.zone='9910308';
+                s.src='https://forfrogadiertor.com/tag.min.js';
+              })(document.body.appendChild(document.createElement('script')));
+            `,
+          }}
+        />
+        <Script
+          id="zone-9915615"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(s){
+                s.dataset.zone='9915615';
+                s.src='https://forfrogadiertor.com/tag.min.js';
+              })(document.body.appendChild(document.createElement('script')));
+            `,
           }}
         />
 
         {/* Epic tag Push Notifications - 9915614 */}
-        <script
+        <Script
+          id="zone-9915614"
+          strategy="afterInteractive"
           dangerouslySetInnerHTML={{
-            __html: `(function(s){
-              s.dataset.zone='9915614';
-              s.src='https://forfrogadiertor.com/tag.min.js';
-            })([document.documentElement, document.body].filter(Boolean).pop().appendChild(document.createElement('script')));`,
+            __html: `
+              (function(s){
+                s.dataset.zone='9915614';
+                s.src='https://forfrogadiertor.com/tag.min.js';
+              })(document.body.appendChild(document.createElement('script')));
+            `,
           }}
         />
-          <script>(function(s){s.dataset.zone='9915679',s.src='https://gizokraijaw.net/vignette.min.js'})([document.documentElement, document.body].filter(Boolean).pop().appendChild(document.createElement('script')))</script>
+
+        {/* Gizokraijaw Push - 9915679 */}
+        <Script
+          id="zone-9915679"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(s){
+                s.dataset.zone='9915679';
+                s.src='https://gizokraijaw.net/vignette.min.js';
+              })(document.body.appendChild(document.createElement('script')));
+            `,
+          }}
+        />
       </Head>
       <body>
         <Main />

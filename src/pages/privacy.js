02c58e1460141c2f913ee01ea6ffc0096d9e8e3f
NewtalentsG <newtalents403@gmail.com>
2025-09-24 22:10:20 +0200

    Update privacy.js

--- src/pages/privacy.js
+++ src/pages/privacy.js
@@ -1,6 +1,17 @@
 // pages/privacy.js
+import React from "react";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
 import PrivacyPolicy from "../components/PrivacyPolicy";
 
 export default function PrivacyPage() {
-  return <PrivacyPolicy />;
+  return (
+    <>
+      <Header />
+      <main>
+        <PrivacyPolicy />
+      </main>
+      <Footer />
+    </>
+  );
 }

e2c2ec85c14d14ce5c2f95183022505ea5c98f52
NewtalentsG <newtalents403@gmail.com>
2025-10-25 00:13:51 +0200

    Refactor app component to use GoogleTranslateDropdown

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -4,37 +4,30 @@ import Script from "next/script";
 import Head from "next/head";
 import 'video.js/dist/video-js.css';
 import NotificationChecker from "../components/NotificationChecker";
-
-// Import language provider
-import { LanguageProvider } from "../components/LanguageProvider";
-import LanguageSelector from "../components/LanguageSelector";
+import GoogleTranslateDropdown from "../components/GoogleTranslateDropdown";
 
 export default function App({ Component, pageProps }) {
   return (
-    <LanguageProvider>
-      <>
-        {/* Title ya website */}
-        <Head>
-          <title>New Talents Stories Group</title>
-          <link rel="icon" href="/logo.png" />
-        </Head>
+    <>
+      <Head>
+        <title>New Talents Stories Group</title>
+        <link rel="icon" href="/logo.png" />
+      </Head>
 
-        {/* Google AdSense script global */}
-        <Script
-          id="adsense-script"
-          async
-          strategy="afterInteractive"
-          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2059573562083386"
-          crossOrigin="anonymous"
-        />
+      {/* Google AdSense */}
+      <Script
+        id="adsense-script"
+        async
+        strategy="afterInteractive"
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2059573562083386"
+        crossOrigin="anonymous"
+      />
 
-        {/* Language Selector (urashobora kuyishyira kuri Navbar cyangwa Footer) */}
-        <LanguageSelector />
+      {/* Dropdown ya Google Translate (igaragara kuri pages zose) */}
+      <GoogleTranslateDropdown />
 
-        {/* Izi component zose zizaba mu rurimi rwahiswemo */}
-        <NotificationChecker />
-        <Component {...pageProps} />
-      </>
-    </LanguageProvider>
+      <NotificationChecker />
+      <Component {...pageProps} />
+    </>
   );
 }

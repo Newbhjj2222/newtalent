a0853343722d83f8d0dce441368e8dc9c88b03c1
NewtalentsG <newtalents403@gmail.com>
2026-01-19 11:11:51 +0200

    Create source.js

---
+++ src/locales/source.js
@@ -0,0 +1,9 @@
+module.exports = {
+  home_title: "Inkuru nshya",
+  home_desc: "Iyi platform ifasha abanditsi gusangiza inkuru zabo isi yose",
+  login: "Injira",
+  register: "Iyandikishe",
+  about: "Ibyerekeye twe",
+  contact: "Twandikire",
+  welcome: "Welcome to New Talents Stories Group",
+};

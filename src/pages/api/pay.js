1dec5b304587a408572ff5fe563245d9b88e5770
NewtalentsG <newtalents403@gmail.com>
2026-01-22 10:52:23 +0200

    Update pay.js

--- src/pages/api/pay.js
+++ src/pages/api/pay.js
@@ -16,7 +16,7 @@ export default async function handler(req, res) {
     // =========================
     // 🔴 PAWAPAY LIVE API KEY
     // =========================
-    const PAWAPAY_API_KEY = "SHYIRAMO_LIVE_API_KEY_YAWE_HANO";
+    const PAWAPAY_API_KEY = "eyJraWQiOiIxIiwiYWxnIjoiRVMyNTYifQ.eyJ0dCI6IkFBVCIsInN1YiI6IjIwMTgiLCJtYXYiOiIxIiwiZXhwIjoyMDgwMzgxOTU2LCJpYXQiOjE3NjQ4NDkxNTYsInBtIjoiREFGLFBBRiIsImp0aSI6ImI0YWM3MzQ4LWYyNDEtNDVjNy04MmQ1LTI0ZTgwZjVlZmJhNSJ9.8qxWc0Aph9QhrhKcfPXvaFe5l_RzSPjOWsCGFr6W88QpMmcyWwqm7W7M83-UCE4OrM8UQZOncdnx-t1MACbObA";
 
     // =========================
     // 🔴 LIVE PAYMENT PAGE URL
